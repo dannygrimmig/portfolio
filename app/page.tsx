72b6fb0f0f99a6f00764ac0220dff72d5c464ff6
DannyGrimmig <dannygrimmig17@gmail.com>
2024-04-26 16:59:53 -0400

    add some content

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,61 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import * as React from "react";
+
+const CATEGORYS = [
+  {
+    key: "main",
+    image:
+      "https://lh3.googleusercontent.com/a/ACg8ocJNMLmcmCb85fXFYSqaz75U4uz2-JloN5xNO4-AtrPVu4FWvk_c5g=s576-c-no",
+    text: "Hi! Here is a quick site about me. Threw together a bit about my background, including my education at Colgate and professional experience as a Software Developer at Toast",
+  },
+  {
+    key: "colgate",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/thumb/d/dc/Colgate_University_Seal_2018.svg/1200px-Colgate_University_Seal_2018.svg.png",
+    text: "I studied Computer Science and Economics at Colgate University. Should put some more information in here too not sure what else to say for now",
+  },
+  {
+    key: "toast",
+    image: "https://static.stocktitan.net/company-logo/tost.png",
+    text: "During my tenure at Toast, I transitioned from an engineering intern to a full-time software engineer, spearheading projects including product education pages, accesible component libraries, and designing and building the prospective e-commerce flow.",
+  },
+];
 
 export default function Home() {
+  // managed
+  const [currentCategory, setCurrentCategory] = React.useState(
+    CATEGORYS.find((category) => category.key === "main")
+  );
+
   return (
     <main className="sm:grid grid-cols-5 min-h-[calc(100vh-74px)]">
-      <div className="sm:col-span-2 lg:col-span-3 grid grid-rows-2 grid-flow-col gap-4 min-h-full p-8 border-r">
-        <div className="rounded-md col-span-2 row-span-2 bg-slate-200">01</div>
-        <div className="rounded-md row-span-1 col-span-1 bg-slate-200">02</div>
-        <div className="rounded-md row-span-1 col-span-1 bg-slate-200">03</div>
+      <div className="sm:col-span-3 border-r">
+        <div className="w-full h-full p-16 grid grid-cols-10 gap-4">
+          <div className="col-span-2 flex flex-col items-end gap-4">
+            {CATEGORYS.map((category) => (
+              <button
+                type="button"
+                onClick={() => setCurrentCategory(category)}
+                key={category.key}
+                className="rounded-md p-4 bg-slate-100 h-24 w-24 flex justify-center items-center"
+              >
+                <img src={category.image} className="rounded-md" />
+              </button>
+            ))}
+          </div>
+
+          <div className="col-span-8 bg-slate-100 p-2 sm:p-4 md:p-8 rounded-lg h-[500px] flex justify-center">
+            <img
+              src={currentCategory?.image}
+              className="rounded-lg object-cover"
+            />
+          </div>
+        </div>
       </div>
 
-      <div className="sm:col-span-3 lg:col-span-2 px-4 pt-8 flex flex-col font-mono gap-12">
+      <div className="sm:col-span-2 px-4 pt-8 flex flex-col font-mono gap-12">
         <div className="flex flex-col">
           <p className="px-4 py-2 rounded-full bg-sky-200 w-max self-end">
             Frontend Engineer
@@ -19,13 +64,7 @@ export default function Home() {
           <h1 className="text-6xl">Grimmig</h1>
         </div>
 
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio
-          nesciunt necessitatibus laborum amet quis eveniet deleniti, omnis
-          ducimus. Molestias, eum dignissimos ducimus, quas obcaecati enim
-          temporibus alias earum rem eveniet quis voluptatibus, maiores
-          doloremque ratione.
-        </p>
+        <p>{currentCategory?.text}</p>
 
         <div className="flex gap-2">
           <Link href="/portfolio">
--- app/ui/nav.tsx
+++ app/ui/nav.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export function NavBar() {
   return (
-    <nav className="border-b py-4 px-8 flex gap-16 items-end">
+    <nav className="font-mono border-b py-4 px-8 flex gap-16 items-end">
       <Link href="/">
-        <h1 className="font-thin text-4xl">Danny</h1>
+        <h1 className="text-4xl">Danny &#128075;</h1>
       </Link>
-      <ul className="flex gap-8 font-mono">
+      <ul className="flex gap-8">
         {LINKS.map((link) => (
           <Link key={link} href={`/${link}`}>
             {link}
