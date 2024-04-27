f61778251293f96be8205ba5d342fd77d4947ae5
DannyGrimmig <dannygrimmig17@gmail.com>
2024-04-27 09:44:25 -0400

    complete home

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,9 @@
 "use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import * as React from "react";
-
-const CATEGORYS = [
-  {
-    key: "main",
-    image:
-      "https://lh3.googleusercontent.com/a/ACg8ocJNMLmcmCb85fXFYSqaz75U4uz2-JloN5xNO4-AtrPVu4FWvk_c5g=s576-c-no",
-    text: "Hi! Here is a quick site about me. Threw together a bit about my background, including my education at Colgate and professional experience as a Software Developer at Toast",
-  },
-  {
-    key: "colgate",
-    image:
-      "https://upload.wikimedia.org/wikipedia/en/thumb/d/dc/Colgate_University_Seal_2018.svg/1200px-Colgate_University_Seal_2018.svg.png",
-    text: "I studied Computer Science and Economics at Colgate University. Should put some more information in here too not sure what else to say for now",
-  },
-  {
-    key: "toast",
-    image: "https://static.stocktitan.net/company-logo/tost.png",
-    text: "During my tenure at Toast, I transitioned from an engineering intern to a full-time software engineer, spearheading projects including product education pages, accesible component libraries, and designing and building the prospective e-commerce flow.",
-  },
-];
+import { CATEGORYS } from "./lib/home-data";
 
 export default function Home() {
   // managed
@@ -32,8 +14,8 @@ export default function Home() {
   return (
     <main className="sm:grid grid-cols-5 min-h-[calc(100vh-74px)]">
       <div className="sm:col-span-3 border-r">
-        <div className="w-full h-full p-16 grid grid-cols-10 gap-4">
-          <div className="col-span-2 flex flex-col items-end gap-4">
+        <div className="w-full h-full p-16 flex flex-col-reverse md:grid md:grid-cols-10 gap-4">
+          <div className="col-span-2 flex md:flex-col items-end gap-4">
             {CATEGORYS.map((category) => (
               <button
                 type="button"
@@ -46,11 +28,21 @@ export default function Home() {
             ))}
           </div>
 
-          <div className="col-span-8 bg-slate-100 p-2 sm:p-4 md:p-8 rounded-lg h-[500px] flex justify-center">
-            <img
-              src={currentCategory?.image}
-              className="rounded-lg object-cover"
-            />
+          <div className="col-span-8 bg-slate-100 p-2 sm:p-4 md:p-8 rounded-lg h-[500px] flex flex-col ">
+            <div className="flex-1 h-5/6 self-center place-self-center">
+              <img
+                src={currentCategory?.image}
+                className="rounded-lg object-fit h-full"
+              />
+            </div>
+
+            <div className="flex-1 content-end font-mono">
+              <h3 className="font-bold">{currentCategory?.company}</h3>
+              <p>{currentCategory?.subhead}</p>
+              {currentCategory?.positions?.map((position, i) => (
+                <li key={i}>{position}</li>
+              ))}
+            </div>
           </div>
         </div>
       </div>
@@ -60,11 +52,14 @@ export default function Home() {
           <p className="px-4 py-2 rounded-full bg-sky-200 w-max self-end">
             Frontend Engineer
           </p>
-          <h1 className="text-6xl">Danny</h1>
-          <h1 className="text-6xl">Grimmig</h1>
+          <h1 className="text-6xl">
+            Danny
+            <br />
+            Grimmig
+          </h1>
         </div>
 
-        <p>{currentCategory?.text}</p>
+        <Typewriter text={currentCategory?.text ?? ""} />
 
         <div className="flex gap-2">
           <Link href="/portfolio">
@@ -96,4 +91,24 @@ export function Button(props: ButtonProps) {
       {text}
     </h3>
   );
+}
+
+function Typewriter({ text }: { text: string }) {
+  // managed
+  const [index, setIndex] = React.useState(0);
+
+  React.useEffect(() => {
+    if (index < text.length) {
+      const timeout = setTimeout(() => {
+        setIndex((prevIndex) => prevIndex + 1);
+      }, 25);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [text, index]);
+
+  // derived
+  const displayedText = text.slice(0, index);
+
+  return <p>{displayedText}</p>;
 }
