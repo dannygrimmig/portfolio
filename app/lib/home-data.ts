8451a88b2df9b6b98ae82c64d6a91b52d241df7f
DannyGrimmig <dannygrimmig17@gmail.com>
2024-06-15 09:13:05 -0400

    next insurance

--- app/lib/home-data.ts
+++ app/lib/home-data.ts
@@ -12,7 +12,6 @@ const MAIN_CATEGORY: Category = {
   image: profileimage,
   text: "Hello! Welcome to my website - click around to get a view into my experiences and favorite projects!",
   badges: [BADGE_MAP.software],
-  className: "md:row-span-2",
 };
 
 const COLGATE_CATEGORY: Category = {
@@ -46,5 +45,5 @@ export const HOME_CATEGORIES = [
   MAIN_CATEGORY,
   COLGATE_CATEGORY,
   TOAST_CATEGORY,
-  // NEXT_CATEGORY,
+  NEXT_CATEGORY,
 ];
